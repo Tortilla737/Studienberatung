ca90ae0a95050e7cf3e672183347364fcc159fce
Gerrit Grieger <gerrit.grieger@haw-hamburg.de>
2016-04-17 14:38:45 +0200

    Fragenmethoden implementiert

--- Studienberatung.java
+++ Studienberatung.java
@@ -1,11 +1,42 @@
 import java.util.*;
 
 public class Studienberatung{
+	Scanner scanner = new Scanner(System.in);
 	public static void main(String[]args){
-		Scanner scanner = new Scanner(System.in);
-		
+		Studienberatung main = new Studienberatung();
+		main.Frage1();
+	}
+	public void Frage1(){
 		System.out.println("Hast du Abitur?");
+		String Antwort1 = scanner.next();
 		
-		String Antwort1 = scanner.nextln();
+		if(Antwort1.equalsIgnoreCase("n")){
+			Frage2();
+		}
+		else if(Antwort1.equalsIgnoreCase("j")){
+			
+		}
+		else{
+			System.out.println("<j> oder <n> Antworten");
+			Frage1();
+		}
+	}
+	public void Frage2(){
+		System.out.println("Hast du Fachabitur?");
+		String Antwort1 = scanner.next();
+		
+		if(Antwort1.equalsIgnoreCase("n")){
+			Frage3();
+		}
+		else if(Antwort1.equalsIgnoreCase("j")){
+			
+		}
+		else{
+			System.out.println("<j> oder <n> Antworten");
+			Frage2();
+		}
+	}
+	public void Frage3(){
+		
 	}
 }
